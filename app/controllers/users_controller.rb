4a6166d46518448ca78e0247b8ebda0c3c89f5a4
黒田学人 <a7108100@gmail.com>
2019-04-06 11:18:01 +0900

    refactor: remove index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    u = User.new(
-      status: :confirmed,
-      name: 'manato kuroda',
-      email: 'hey@gmail.com',
-      phone: '0920205020'
-    )
-    u.save!
-
-    render json: { ok: true, test: 'hi' }, status: :ok
-  end
-
   def search
     user_search = UserSearch.new(search_params)
     @users = user_search.search
